03e834a3f792be2440e7adf9fc86c65f972e887f
Dans182 <dgaiteiro@gmail.com>
2023-11-28 16:05:28 +0100

    refactor del codigo

--- Backend/FastAPI/routers/users.py
+++ Backend/FastAPI/routers/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 #pydantic nos ayuda a definir una entidad
 
-router = APIRouter()
+router = APIRouter(prefix = "/users", tags = ["users"], responses = {404: {"message": "No encontrado"}})
 
 # Creamos la entidad User()
 class User(BaseModel): # Este Basemodel como parámetro, nos está dando la capacidad de crear una entidad
@@ -16,7 +16,7 @@ users_list = [User(id = 1, name = "Daniel", surname = "Moure", url = "https://ww
               User(id = 2, name = "Euge", surname = "Bordenave", url = "https://www.google.com", age = 25),
               User(id = 3, name = "Reck", surname = "Jowar", url = "https://www.google.com", age = 30)]
 
-@router.get("/usersjson")
+@router.get("/json")
 async def usersjson():
     return [{"name": "Daniel", "surname": "Gaiteiro", "url": "https:www.google.com", "age": 20},
             {"name": "Euge", "surname": "Bordenave", "url": "https:www.google.com", "age": 25},
@@ -24,7 +24,7 @@ async def usersjson():
 
 # Los JSON es una forma de estructurar datos que todos lo entiendan. Servidores, clientes, etc
 
-@router.get("/users")
+@router.get("/")
 async def users():
     return users_list
 
