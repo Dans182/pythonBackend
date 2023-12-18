2729873c576e45835d066e3b56ca76c6eccd29d8
Dans182 <dgaiteiro@gmail.com>
2023-12-18 13:16:13 +0100

    post db listo
    undefined

--- Backend/FastAPI/routers/user_db.py
+++ Backend/FastAPI/routers/user_db.py
@@ -7,21 +7,19 @@ router = APIRouter(prefix = "/userdb",
                    tags = ["userdb"], 
                    responses = {status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
 
-def search_user(id: int):
-    users = filter(lambda user: user.id == id, users_list) #Tiene primero un objeto user. Dentro de ese objeto, queremos que compare el campo id de esa clase user, con el ID que se para como parametro. A mayores le pasamos para que itere en la lista, por eso el users_list
+def search_user_by_email(email: str):
     try:
-        return list(users)[0] #El filter puede devolvernos varios objetos, es por esa razón que creamos una lista. En esa lista, le pasamos la variable users que nace a razón del filter
+        user = db_client.local.users.find_one({"email": email}) #Aca lo busco en DB
+        return User(**user_schema(user))
     except:
-        return {"error": "No se ha encontrado el usuario"}
+        return {"error": "No se ha encontrado el usuario"} #aca creamos la transformacion a diccionario y creamos el objeto user
 
 #POST
-#Crear nuevo usuario
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED) 
 async def user(user: User):
-    # if type(search_user(user.id)) == User:
-    #     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
-    # else:
-
+    if type(search_user_by_email(user.email)) == User:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail="El usuario ya existe")
     user_dict = dict(user)
     del user_dict["id"] #Al dejarlo opcional en el schema, si no le pasamos ID lo crea como un null. Es por eso que mandamos a borrar el id, para que mongo me autogenere el ID
     id = db_client.local.users.insert_one(user_dict).inserted_id
