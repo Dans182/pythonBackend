08c20aba1cef749852413995ff97c9ed7f957010
Dans182 <dgaiteiro@gmail.com>
2023-12-18 15:51:55 +0100

    mongodb listo. Api done

--- Backend/FastAPI/routers/user_db.py
+++ Backend/FastAPI/routers/user_db.py
@@ -46,17 +46,21 @@ async def user(id: str):
     return search_user("_id", ObjectId(id))
 
 #PUT
-@router.put("/", status_code=status.HTTP_204_NO_CONTENT)
-async def user(id: str):
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+@router.put("/", response_model=User)
+async def user(user: User):
 
-    if not found:
+    user_dict = dict(user)
+    del user_dict["id"]
+
+    try:
+        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+    except: 
         return {"error": "No se ha actualizado el usuario"}
-    else:
-        return user
+    
+    return search_user("_id", ObjectId(user.id))
 
 #DELETE
-@router.delete("/{id}")
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def user(id: str):
     found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
 
